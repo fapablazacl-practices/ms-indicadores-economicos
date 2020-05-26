7941053fa67d65d6ac41592a67c8b85d0cf69953
Felipe Apablaza <ing.apablaza@gmail.com>
2020-05-26 18:41:02 -0400

    Agregado endpoint GET de prueba

--- src/index.js
+++ src/index.js
@@ -1,4 +1,13 @@
 
 const express = require('express');
+const app = express();
 
+app.get('/', (request, response) => {
+    response.send({
+        "value": "true"
+    });
+});
 
+const server = app.listen(8000, () => {
+    console.log(`Ejecutando en ${host}:${port}`);
+});
