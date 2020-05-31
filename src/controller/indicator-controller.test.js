b5bc5583ea78528fa0147ee050a225f184ec327b
Felipe Apablaza <ing.apablaza@gmail.com>
2020-05-30 21:11:04 -0400

    Agregada prueba unitaria inicial asociada al controlador

---
+++ src/controller/indicator-controller.test.js
@@ -0,0 +1,11 @@
+
+const assert = require('assert');
+const mockery = require('mockery');
+
+describe('IndicatorController', () => {
+    describe('get', () => {
+        it('should return the data from the service formatted', (done) => {
+            done();
+        });
+    });
+});
