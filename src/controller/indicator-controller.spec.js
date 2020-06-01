45da1f14c4e61a29ddbac23acf0cc80e9e7610ef
Felipe Apablaza <ing.apablaza@gmail.com>
2020-05-31 20:12:07 -0400

    Correccion registro de mock via mockery

--- src/controller/indicator-controller.spec.js
+++ src/controller/indicator-controller.spec.js
@@ -1,11 +1,64 @@
 
-const assert = require('assert');
 const mockery = require('mockery');
+const expect = require('mocha').expect;
+
+const { indeconResponseMock } = require('./indecon-response-mock');
+
+class IndeconServiceMock {
+    async getIndicatorHistory(indicator) {
+        console.log("Calling,mock!");
+
+        return new Promise( (resolve, reject) => {
+            resolve(indeconResponseMock);
+        });
+    }
+
+    static getAvailableIndicators() {
+        return ['oro'];
+    }
+}
 
 describe('IndicatorController', () => {
-    describe('get', () => {
-        it('should return the data from the service formatted', (done) => {
-            done();
+    describe('getStats', () => {
+        before( () => {
+            mockery.registerMock('./services/indecon-service', { IndeconService: IndeconServiceMock});
+            mockery.enable();
+        });
+
+        after( () => {
+            mockery.deregisterAll();
+            mockery.disable();
+        });
+
+        it('should compute correctly the min, max and average stats', (done) => {
+            const { IndicatorController } = require('./indicator-controller');
+            const controller = new IndicatorController();
+
+            controller.getStats('oro')
+                .then( (result) => {
+                    expect(result).to.be.not.null;
+                    done();
+                })
+                .catch( (err) => {
+                    console.log("Faiil");
+                    expect(false).to.be.true;
+                    done();
+                });
+        });
+
+        xit('should raise error when supplied with an invalid indicator name', (done) => {
+            const { IndicatorController } = require('./indicator-controller');
+            const controller = new IndicatorController();
+
+            controller.getStats('platta')
+                .then( (result) => {
+
+                    done();
+                })
+                .catch( (err) => {
+                    expect(err).to.be.true;
+                    done();
+                });
         });
     });
 });
